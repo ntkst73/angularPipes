f8a5c67f27a8b0761067ab5319c6e9913477340b
Кошарный Никита <ntkst73@mail.ru>
2021-01-25 11:46:05 +0400

    pipes search and Date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs';
 
+export interface Post {
+  title: string
+  text:string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-part3';
+
+  promise: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('Promise Resolve')
+    },4000)
+  })
+  date: Observable<Date> = new Observable( obs => {
+    setInterval( () => {
+      obs.next(new Date())
+    },1000)
+  })
+  search = ''
+  searchField = 'title'
+
+  posts: Post[] = [
+    {title: 'Beer', text: 'Самое лучшее пиво в мире'},
+    {title: 'Bread', text: 'The best bread in the world'},
+    {title: 'Javascript', text: 'The best language in the world'}
+  ]
+
+  addPost() {
+    this.posts.unshift({
+      title: 'Angular 8',
+      text: 'Angular 8 course'
+    })
+  }
 }
